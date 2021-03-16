1386b23003e07782c73425f57c7499cee4920fd0
Nikita Lobanov <lobanov_2000_nikita@mail.ru>
2021-03-17 00:04:53 +0300

    add new pattern from book headfirst: state

---
+++ State/src/bookHeadFirst/gumballstate/State.java
@@ -0,0 +1,11 @@
+package bookHeadFirst.gumballstate;
+
+public interface State {
+ 
+	public void insertQuarter();
+	public void ejectQuarter();
+	public void turnCrank();
+	public void dispense();
+	
+	public void refill();
+}
---
+++ State/src/bookHeadFirst/gumballstatewinner/State.java
@@ -0,0 +1,11 @@
+package bookHeadFirst.gumballstatewinner;
+
+public interface State {
+ 
+	public void insertQuarter();
+	public void ejectQuarter();
+	public void turnCrank();
+	public void dispense();
+	
+	public void refill();
+}
